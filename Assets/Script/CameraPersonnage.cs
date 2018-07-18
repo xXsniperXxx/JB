62e05f3e4d49977d1e3cd1709ee03c01fcd639ef
Unknown <baudin.jeanba@gmail.com>
2018-07-18 14:06:44 +0200

    Personnage se dedplace dans l espace cammera

--- Assets/Script/CameraPersonnage.cs
+++ Assets/Script/CameraPersonnage.cs
@@ -5,8 +5,9 @@ public class CameraPersonnage : MonoBehaviour
 {
 	#region Variables (public)
 	public PersonnageJoueurbg m_pTarget = null;// p = pointeur, il peut etre 0
+	static public CameraPersonnage Instance = null;
 	public float m_fDistanceDeSuivi = 0.0f;
-
+	public float m_fVitesseDeRotation = 0.0f;
 	#endregion
 
 	#region Variables (private)
@@ -15,20 +16,66 @@ public class CameraPersonnage : MonoBehaviour
 
 	#endregion
 
+	private void Awake()
+	{
+		if (CameraPersonnage.Instance != null)
+		{
+			if (CameraPersonnage.Instance != this)
+
+			Debug.LogError("Attention,il y a deux CameraPersonnage dans la scene");
+				Destroy(this);
+
+			return;
+		}
+
+		CameraPersonnage.Instance = this;
+	}
+
+		private void Update()
+	{
+		TournerCamera();
+
+	}
+
+
 	private void LateUpdate()
 	{
 		if (m_pTarget != null)
-		Follow();
-// pour jmp : nouvelle fonction , input. Getbutton(down)("Jump") 
+			Follow();
+		// pour jmp : nouvelle fonction , input. Getbutton(down)("Jump") 
+
 	}
 
 	private void Follow()
 	{
-		Vector3 tNouvellePosition = m_pTarget.transform.position + Vector3.up - (transform.forward) * m_fDistanceDeSuivi;
+		
 		// Gameobject Get : Tres utile 
-		transform.position = tNouvellePosition;
+		
+		
+		Vector3 tNouvellePositionCamera = m_pTarget.transform.position + Vector3.up - (transform.forward * m_fDistanceDeSuivi);
+		transform.position = tNouvellePositionCamera;
+	}
+
+
+	private void TournerCamera()
+	{
+		float fMouseX = Input.GetAxis("Mouse X");
+
+		if (fMouseX != 0.0f)
+		
+			transform.RotateAround(m_pTarget.transform.position, Vector3.up, fMouseX * (m_fVitesseDeRotation * Time.deltaTime));
+		
+
+		float fMouseY = Input.GetAxis("Mouse Y");
+
+		if (fMouseY != 0.0f)
+		
+		
+			transform.RotateAround(m_pTarget.transform.position, transform.right, fMouseY * (-m_fVitesseDeRotation * Time.deltaTime));
+
+			
 		
 
 	}
-}
 
+}
--- Assets/Script/PersonnageJoueurbg.cs
+++ Assets/Script/PersonnageJoueurbg.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 public class PersonnageJoueurbg : MonoBehaviour
 {
 	#region Variables (public)
-
+	
 	public Rigidbody m_pRigidbody = null;
-
 	public float m_fHp = 10;
-
 	public float m_fSpeed = 20.0f;
 	public float m_fVitesseDeSaut = 200.0f;
 	#endregion
 
 	#region Variables (private)
 
-
-
 	#endregion
-
-
 
 
 	private void Update()
@@ -38,7 +32,13 @@ public class PersonnageJoueurbg : MonoBehaviour
 		if (tDirection != Vector3.zero)
 		{
 
-			tDirection.Normalize();
+			
+			tDirection = CameraPersonnage.Instance.transform.TransformDirection(tDirection);
+			tDirection.y = 0.0f;
+			if (tDirection.sqrMagnitude != 0.0f)
+				tDirection.Normalize();
+			else
+				tDirection = transform.forward; 
 
 			Vector3 tDeplacement = tDirection * (m_fSpeed * Time.deltaTime); // deplacement de 1 en profondeur 
 			m_pRigidbody.MovePosition (transform.position + tDeplacement);
