8e8479a3cd9adc09f752624cd7b85ae2a14cbe85
Unknown <baudin.jeanba@gmail.com>
2018-07-18 09:09:52 +0200

    Jump

--- Assets/Script/PersonnageJoueurbg.cs
+++ Assets/Script/PersonnageJoueurbg.cs
@@ -5,10 +5,12 @@ public class PersonnageJoueurbg : MonoBehaviour
 {
 	#region Variables (public)
 
+	public Rigidbody m_pRigidbody = null;
+
 	public float m_fHp = 10;
 
 	public float m_fSpeed = 20.0f;
-
+	public float m_fVitesseDeSaut = 200.0f;
 	#endregion
 
 	#region Variables (private)
@@ -23,7 +25,7 @@ public class PersonnageJoueurbg : MonoBehaviour
 	private void Update()
 	{
 		MoveCharacter();
-
+		Jump();
 	}
 
 	private void MoveCharacter()
@@ -33,25 +35,29 @@ public class PersonnageJoueurbg : MonoBehaviour
 
 		Vector3 tDirection = new Vector3(fHorizontal, 0.0f, fVertical);
 
-
-		
-		
-
 		if (tDirection != Vector3.zero)
 		{
 
 			tDirection.Normalize();
 
 			Vector3 tDeplacement = tDirection * (m_fSpeed * Time.deltaTime); // deplacement de 1 en profondeur 
-
-			transform.position += tDeplacement;
+			m_pRigidbody.MovePosition (transform.position + tDeplacement);
+			
 
 			transform.forward = tDirection;
+		}
+	}
 
-
+	private void Jump()
+	{
+		if (Input.GetButtonDown("Jump"))
+		{
+			Vector3 tSaut = Vector3.up * m_fVitesseDeSaut;
+			m_pRigidbody.AddForce(tSaut, ForceMode.Impulse);
 		}
 
-
 	}
+
+
 
 }
