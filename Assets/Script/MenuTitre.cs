dc369ada91017c2666544f8ebf6382ed4075287d
Unknown <baudin.jeanba@gmail.com>
2018-07-19 15:15:24 +0200

    Creation du Menu du meilleur jeu du monde

---
+++ Assets/Script/MenuTitre.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTitre : MonoBehaviour
+{
+	#region Variables (public)
+
+	public string m_sNomDeLaScene = null;
+	
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+	public void LancerJeu()
+	{
+		SceneManager.LoadScene(m_sNomDeLaScene);
+	}
+
+	public void QuittezJeu()
+	{
+		Application.Quit();
+	}
+
+}
